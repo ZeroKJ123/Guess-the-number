db3abf2bb56e35ccaf44f2037e7e1c036490fcfb
Zero <valzorian.exe@gmail.com>
2024-07-08 15:36:48 +0300

    Добавил выбор чисел

--- main.cpp
+++ main.cpp
@@ -3,11 +3,24 @@
 
 using namespace std;
 
+int Jopa(int min, int max)
+{
+    return (rand() % (max - min) + min);
+}
+
 int main()
 {
     srand(time(NULL));
 
-    int randNum = rand() % 100;
+    int min;
+    int max;
+
+    cout << "Введите нижнее число: ";
+    cin >> min;
+    cout << "Введите верхнее число: ";
+    cin >> max;
+
+    int randNum = Jopa(min, max);
     bool check = true;
 
     while(check)
@@ -35,3 +48,4 @@ int main()
         
     }
 }
+
