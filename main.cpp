cef1087dc2255984ba2dee73e2c965b5b698e49b
Zero <valzorian.exe@gmail.com>
2024-07-08 15:26:17 +0300

    Сделал первую версию игры

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    srand(time(NULL));
+
+    int randNum = rand() % 100;
+    bool check = true;
+
+    while(check)
+    {
+        int anw;
+        cout << "Ваше число: ";
+        cin >> anw;
+        if(anw == randNum)
+        {
+            cout << "Вы победили!" << endl;
+            check = false;
+        }
+        else if(anw == 9954)
+        {
+            cout << randNum << endl;
+        }
+        else if(anw > randNum)
+        {
+            cout << "Ваше число больше загаданого" << endl;
+        }
+        else if(anw < randNum)
+        {
+            cout << "Ваше число меньше загаданого" << endl;
+        }
+        
+    }
 }
